f11d9b90c264f8d231f021686c303be556e39911
Raja Madupu <rajamadupu@outlook.com>
2024-05-07 18:49:04 -0400

    changes to course-search-scene.fxml and CoursesMainSceneController

--- src/main/java/edu/virginia/sde/reviews/CoursesMainSceneController.java
+++ src/main/java/edu/virginia/sde/reviews/CoursesMainSceneController.java
@@ -3,14 +3,29 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.VBox;
 
+import java.awt.*;
+
 public class CoursesMainSceneController {
 
    @FXML
-   private Vbox coursesContainer;
+   private VBox coursesContainer;
+   @FXML
+   private TextField courseMnemonicTextField;
+   @FXML
+   private TextField courseNumberTextField;
+   @FXML
+   private TextField courseTitleTextField;
+
 
    public void initalize() {
       //TODO: Implement
    }
+
+   public void handleLogOutButton() {}
+
+   public void handleMyReviewsButton(){}
+
+
 
 
 }
